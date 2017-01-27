6f484cb3ac3df4ebdab0976e06d048c07cdac15d
nick <nick@androidstudio.test>
2017-01-27 16:47:54 +0000

    Initial commit

---
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.whitelegg_n.osmdroid1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //important! set your user agent to prevent getting banned from the osm servers
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        setContentView(R.layout.activity_main);
+        MapView mv = (MapView)findViewById(R.id.map1);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+    }
+}
