9b378367ddb8550d00e0fe252dd3208c5960316e
whitelegg_n <nickw4426@gmail.com>
2017-02-08 17:08:13 +0000

    Added 2nd activity with buttons - they don't do anything yet though

--- app/src/main/java/com/example/whitelegg_n/osmdroid1/MainActivity.java
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/MainActivity.java
@@ -1,20 +1,24 @@
 package com.example.whitelegg_n.osmdroid1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         //important! set your user agent to prevent getting banned from the osm servers
@@ -22,14 +26,25 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
         MapView mv = (MapView)findViewById(R.id.map1);
-        mv.setTileSource(TileSourceFactory.CYCLEMAP);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1, 22.5));
     }
 
-    public boolean onCreateOptionsMenu (Menu menu) {
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_hello_map, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId()==R.id.choosemap)
+        {
+            Intent intent = new Intent(this, MapChooseActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/MapChooseActivity.java
@@ -0,0 +1,17 @@
+package com.example.whitelegg_n.osmdroid1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by whitelegg_n on 08/02/2017.
+ */
+
+public class MapChooseActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mca);
+    }
+}
