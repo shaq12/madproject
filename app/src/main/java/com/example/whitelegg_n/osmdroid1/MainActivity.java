57fc63fc4a9fb314ab69f471f06ce2a9cfc07412
whitelegg_n <nickw4426@gmail.com>
2017-02-10 12:58:05 +0000

    Added comments

--- app/src/main/java/com/example/whitelegg_n/osmdroid1/MainActivity.java
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/MainActivity.java
@@ -31,33 +31,60 @@ public class MainActivity extends Activity
         mv.getController().setCenter(new GeoPoint(40.1, 22.5));
     }
 
+    // onCreateOptionsMenu()
+    // Runs automatically on startup, loads the menu XML file into a Menu object.
     public boolean onCreateOptionsMenu (Menu menu)
     {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_hello_map, menu);
         return true;
     }
+
+    // onOptionsItemSelected()
+    // Responds to menu items.
     public boolean onOptionsItemSelected(MenuItem item)
 
     {
+        // If the menu item was the one with the ID of "choosemap"...
         if (item.getItemId() == R.id.choosemap)
         {
+            // Create an Intent to launch the MapChooseActivity
             Intent intent = new Intent (this, MapChooseActivity.class);
+
+            // Start the Activity using the Intent.
+            // Use a request code of 0 so that onActivityResult() knows which secondary
+            // activity is sending the data back - in a full app, there would be more than
+            // one secondary activity and each would be launched with its own request code.
             startActivityForResult(intent, 0);
             return true;
         }
         return false;
     }
 
+    // onActivityResult()
+    // Runs when we get a response back from the secondary activity.
+    // Parameters:
+    // - requestCode: the request code used to launch the secondary activity (see above)
+    // - responseCode: the response code sent back from the secondary activity (e.g. RESULT_OK)
+    // - intent: the Intent sent back from the secondary activity
 
     public void onActivityResult(int requestCode, int responseCode,Intent intent)
     {
+        // If the secondary activity returned a result of RESULT_OK...
         if(responseCode == RESULT_OK)
         {
+            // If the request code used to launch the secondary activity was 0, it means we launched
+            // MapChooseActivity (see above).
             if(requestCode == 0)
             {
+                // Get the Bundle from the Intent.
                 Bundle bundle = intent.getExtras();
+
+                // Get the cyclemap entry from the Bundle
                 boolean cyclemap = bundle.getBoolean("com.example.cyclemap");
+
+                // Set the map to either the cycle map, or the default map (MAPNIK) depending
+                // on the value of the boolean retrieved from the Bundle.
                 if(cyclemap==true)
                 {
                     mv.setTileSource(TileSourceFactory.CYCLEMAP);
--- app/src/main/java/com/example/whitelegg_n/osmdroid1/MapChooseActivity.java
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/MapChooseActivity.java
@@ -22,18 +22,35 @@ public class MapChooseActivity extends Activity implements View.OnClickListener
         cyclemap.setOnClickListener(this);
     }
 
+    // onClick() runs when the buttons have been pressed
     public void onClick(View view)
     {
+        // Set the cyclemap boolean variable to false by default.
         boolean cyclemap = false;
+
+        // Get the ID of the button that was pressed. If it's the ID for the cyclemap
+        // button, set the cyclemap boolean to true.
         if(view.getId() == R.id.btnCyclemap)
         {
             cyclemap = true;
         }
+
+        // Create an Intent to send information back to the main activity.
         Intent intent = new Intent();
+
+        // Create a Bundle to store data to be sent back.
         Bundle bundle = new Bundle();
+
+        // Add the boolean to the Bundle.
         bundle.putBoolean("com.example.cyclemap", cyclemap);
+
+        // Add the Bundle to the Intent.
         intent.putExtras(bundle);
+
+        // Set an OK result, meaning that everything was successful.
         setResult(RESULT_OK, intent);
+
+        // Close the second activity (this will return to the first)
         finish();
     }
 }
